e880c7354462a5a145b1a3234eac57f143e5b5ae
kimyoosang <yoo99485@gmail.com>
2021-04-08 20:29:37 +0900

    #Add #4 LoginSection

--- src/components/Navigation/Login.tsx
+++ src/components/Navigation/Login.tsx
@@ -12,11 +12,6 @@ const Login: React.FC = () => {
 
     const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
     window.location.assign(kakaoLoginUrl);
-
-    const url = new URL(window.location.href);
-
-    const authorizationCode = url.searchParams.get('code');
-    console.log(authorizationCode);
   };
 
   return (
