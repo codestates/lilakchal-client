16705f51d4ec75ed4d3f1024d465d3b8e789e46b
Pangho297 <72400381+Pangho297@users.noreply.github.com>
2021-04-08 22:42:47 +0900

    fix UserInfo.tsx

--- src/redux/modules/UserInfo.tsx
+++ src/redux/modules/UserInfo.tsx
@@ -1,7 +1,7 @@
 import { createAction, ActionType, createReducer  } from 'typesafe-actions';
 
 //State 정의
-export type userInfoState = {
+export interface userInfoState {
   id: number,
   kakaoId: string,
   name: string,
@@ -13,17 +13,11 @@ export const initialState: userInfoState = {
   name: '',
 };
 
-interface Info {
-  id: number,
-  kakaoId: string,
-  name: string
-}
-
 //Action type 정의
 const userInfo = 'ttmk/UserInfo/userInfo'; //Action은 '앱이름/reducer이름/Acction_type' 형태여야 한다.
 
 //Action 생성자 정의 및 export
-export const UserInfoHandler = createAction(userInfo)<Info>();
+export const UserInfoHandler = createAction(userInfo)<userInfoState>();
 
 const actions = UserInfoHandler;
 
