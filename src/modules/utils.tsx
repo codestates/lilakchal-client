b1df8ac55eb6b8a501d815df89bf56513b094ec9
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-07 20:37:46 -0700

    create timer

---
+++ src/modules/utils.tsx
@@ -0,0 +1,8 @@
+import {convertSecToHourString} from './converters';
+
+export const getRestTime = (endtime: Date) : string => {
+  const today = new Date();
+  const dateDiff = Math.floor((endtime.getTime() - today.getTime()) / 1000);
+
+  return convertSecToHourString(dateDiff);
+};
