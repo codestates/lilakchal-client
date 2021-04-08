7a7f5284575c1c72c65756b5cd3780513c2e28c6
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-08 02:43:42 -0700

    add props in goChat

--- src/components/ItemCard/GoChat.tsx
+++ src/components/ItemCard/GoChat.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import {constantString} from '../../modules/strings';
 
-interface Props extends RouteComponentProps {
+interface Props extends RouteComponentProps{
   itemId: number
 }
 
 const GoChat: React.FC<Props> = ({ history, itemId }) => {
 
-  const goChat = () => history.push({ //https://velog.io/@dhlee91/this.props.history.push%EB%A1%9C-props-%EB%84%98%EA%B2%A8%EC%A3%BC%EA%B8%B0
-    pathname: '/ko/mypage/chat',
-    state: {itemId: 1} //room
+  const goChat = () => history.push('/ko/mypage/chat', {
+    itemId //room
   });
 
   return (
