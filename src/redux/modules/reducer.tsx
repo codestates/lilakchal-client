5748409d2bf37094457f84824d919ac5decdfd46
Pangho297 <monanazzang@gmail.com>
2021-04-09 00:36:55 +0900

    update username.tsx

--- src/redux/modules/reducer.tsx
+++ src/redux/modules/reducer.tsx
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import AccountReducer from './account';
 import InputvalueReducer from './SearchValue';
 import ItemReducer from './Items';
-import UsernameReducer from './username';
 
 const rootReducer = combineReducers({
   AccountReducer,
   InputvalueReducer,
-  ItemReducer,
-  UsernameReducer
+  ItemReducer
 });
 
 export default rootReducer;
