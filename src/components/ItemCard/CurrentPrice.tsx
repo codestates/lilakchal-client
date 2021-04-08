0c9beaebac4f6ac89a28ad65e014c5b5b650bcf8
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-07 17:51:45 -0700

    create currentprice and additinal folder

---
+++ src/components/ItemCard/CurrentPrice.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {units} from '../../modules/units';
+
+interface Props {
+  itemId: number,
+  price: number
+}
+
+const CurrentPrice: React.FC<Props> = ({itemId, price}) => {
+  return (
+    <div className="itemcard-price"  id={`itemcard-${itemId}`}>
+      <span>{price}</span><span>{units.price}</span>
+    </div>
+  );
+};
+
+export default CurrentPrice;
