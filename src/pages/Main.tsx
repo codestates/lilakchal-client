c4ab1738b8e64076133a77adec6378db98d92e61
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-08 09:37:16 -0700

    arrange main layour

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,24 +5,29 @@ import RegisterPage from './RegisterPage';
 import Mypage from './Mypage';
 import Header from '../components/Navigation/Header';
 import { RouterProps, withRouter } from 'react-router';
+import {Content, Container} from './style/MainPageStyle';
 
 
 const Main:React.FC<RouterProps> = ({ history }) => {
 
   return (
     <Router history={history}>
-      <Header/>
-      <Switch>
-        <Route path='/ko/register'>
-          <RegisterPage/>
-        </Route>
-        <Route path='/ko/mypage'>
-          <Mypage/>
-        </Route>
-        <Route path='/ko/search'>
-          <SearchPage/>
-        </Route>
-      </Switch>
+      <Container>
+        <Header/>
+        <Content>
+          <Switch>
+            <Route path='/ko/register'>
+              <RegisterPage/>
+            </Route>
+            <Route path='/ko/mypage'>
+              <Mypage/>
+            </Route>
+            <Route path='/ko/search'>
+              <SearchPage/>
+            </Route>
+          </Switch>
+        </Content>
+      </Container>
     </Router>
   );
 };
---
+++ src/pages/style/SearchPageStyle.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 50%;
+  background-color: #D1B2FF;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
