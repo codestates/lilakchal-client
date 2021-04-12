f43d4b8990bc0ccc735e2c5cdd6d6c35e3a345a0
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-12 09:56:23 -0700

    create Chat

--- src/components/Chat/style/MessageStyle.tsx
+++ src/components/Chat/style/MessageStyle.tsx
@@ -1,23 +1,30 @@
 import styled, {css} from 'styled-components';
 
 export const Container = styled.div<{isMine: boolean}>`
-  height: 100px;
-  max-width: 80%;
   display: flex;
   justify-content: flex-end;
+  overflow: auto;
   ${(props) => props.isMine ? css`
     flex-direction: row-reverse;
-    background-color: #F6F6F6;
   ` : `
     flex-direction: row;
+  `}
+`;
+
+export const Text = styled.span<{isMine: boolean}>`
+  font-size: 1rem;
+  margin: 1rem;
+  padding: 0.5rem;
+  border-radius: 2rem;
+  ${(props) => props.isMine ? css`
+    background-color: #F6F6F6;
+  ` : `
     background-color: #3DB7CC;
   `}
 `;
 
-export const Text = styled.div`
-  font-size: 1rem;
-`;
-
+//div 높이와 line-height 높이를 같게하면 글자는 line-height 가운데 위치하게 된다.
 export const Time = styled.div`
-  font-size: 0.5rem;
+  font-size: 0.8rem;
+  line-height: 4rem;
 `;
