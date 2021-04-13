ecfd529de8ab58e8900540a319342483e6a638b9
EunJeong Lee <sssver28@gmail.com>
2021-04-14 01:43:59 +0900

    fix chat header

--- src/components/ItemCard/GoChat.tsx
+++ src/components/ItemCard/GoChat.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import {constantString} from '../../modules/strings';
+import { useDispatch } from 'react-redux';
+import { HeaderHandler } from '../../redux/modules/HeaderState';
 
 interface Props extends RouteComponentProps{
   itemId: number,
@@ -8,11 +10,16 @@ interface Props extends RouteComponentProps{
 }
 
 const GoChat: React.FC<Props> = ({ history, itemId, title }) => {
-
-  const goChat = () => history.push('/ko/mypage/chat', {
-    itemId, //room
-    title
-  });
+  
+  const dispatch = useDispatch();
+  
+  const goChat = () => {
+    history.push('/ko/mypage/chat', {
+      itemId, //room
+      title
+    });
+    dispatch(HeaderHandler(true));
+  };
 
   return (
     <button className='chat-btn' onClick={goChat}>{constantString.goChat}</button>
