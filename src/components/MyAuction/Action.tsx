4b1d5d4c28ee1e3326f30c1da24284823b44b39d
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-14 02:13:22 +0900

    use route in header to show different menuGroup by case

--- src/components/MyAuction/Action.tsx
+++ src/components/MyAuction/Action.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import { RootStateOrAny, useSelector } from 'react-redux';
 import { Container } from '../../pages/style/SearchPageStyle';
-import { HeaderHandler } from '../../redux/modules/HeaderState';
 import { Item } from '../../redux/modules/Items';
 import ItemCard from '../ItemCard';
 import './style/Auction.scss';
@@ -13,15 +11,8 @@ const Action: React.FC = () => {
 
   const itemState = useSelector((state:RootStateOrAny) => state.ItemReducer);
   const {items} = itemState;
-  const dispatch = useDispatch();
 
-  //페이지 뒤로가기, 앞으로 가기 할때 items바뀌도록 하기
 
-  useEffect(() => {
-    
-    dispatch(HeaderHandler(false));
-    console.log('앞으로 다시오면 mypage useeffect가 실행되나요?'); 
-  }, []);
 
   return (
     <div className='itemCard'>
