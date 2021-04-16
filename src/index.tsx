a9a30e48fd593175d679dccf5170ade1815b1dce
Pangho297 <monanazzang@gmail.com>
2021-04-16 23:03:47 +0900

    add font

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import './style/index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
