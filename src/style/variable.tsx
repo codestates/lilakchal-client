fa40ba023f8b2113e23c6751ee9d258d45b77b51
kimyoosang <yoo99485@gmail.com>
2021-04-22 06:13:03 +0900

    Add Landing responsive css

--- src/style/variable.tsx
+++ src/style/variable.tsx
@@ -1,11 +1,13 @@
 const size = {
   mobile: '600px',
   desktopS: '1152px',
-  desktopM: '1700px'
+  desktopM: '1700px',
+  desktopL: '990px'
 };
 
 export const device = {
   mobile: `(max-width: ${size.mobile})`,
   desktopS: `(max-width: ${size.desktopS})`,
-  desktopM: `(max-width: ${size.desktopM})`
+  desktopM: `(max-width: ${size.desktopM})`,
+  desktopL: `(max-width: ${size.desktopL})`
 };
